d94ed63c69387639b8e696303b4dccecb182a72c
Javi Plaza <javierplazasisques@gmail.com>
2021-05-04 19:11:08 +0200

    Root commit

---
+++ cuadrantes.py
@@ -0,0 +1,9 @@
+class Cuadrante():
+
+    def __init__(self, xmin, ymin, xmax, ymax):
+        print("Cuadrante creado: ", xmin, " ", ymin," ", xmax, " ", ymax)
+        self.xmin = xmin
+        self.ymin = ymin
+        self.xmax = xmax
+        self.ymax = ymax
+    
